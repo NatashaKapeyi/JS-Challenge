c88230a5685d5aff8cd96d65f8cc3e4c9ff3902b
NatashaKapeyi <natashazikhona7@gmail.com>
2023-11-14 16:48:19 +0200

    done

--- script/code.js
+++ script/code.js
@@ -1,1 +1,12 @@
 //Challlenge
+
+let numbers= [12, 18, 11, 5, 8, 2];
+
+function Middle(numbers) {
+    const middle = numbers.length >> 1;
+    return numbers.length & 1
+        ? numbers[middle]
+        : numbers.slice(middle - 1, middle + 1);
+}
+
+console.log(Middle(numbers));
